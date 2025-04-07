c290e4794352006317fe680097be0dea4170bf02
Nick71100 <nick.delattre4@gmail.com>
2025-04-07 14:26:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+class entity {
+  constructor(
+    nameParam,
+    hpParam,
+    powerParam,
+    staminaParam,
+    xpParam,
+    xpMaxParam
+  ) {
+    this.name = nameParam;
+    this.hp = hpParam;
+    this.power = powerParam;
+    this.stamina = staminaParam;
+    this.xp = xpParam;
+    this.xpMax = xpMaxParam;
+  }
+}
